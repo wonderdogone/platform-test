b7e3b83a11eaa33ae21b4849710ca13312c012c9
wonderdogone <splicetv@gmail.com>
2016-04-12 22:30:59 -0700

    remove empty body checker middleware
    not needed on this endpoint

--- src/controllers/router_v1.js
+++ src/controllers/router_v1.js
@@ -18,7 +18,7 @@ const route = express.Router();
 
 
 route.get('/users', v1handler.allUsers);
-route.delete('/users/:id', middle.checkIfEmpty, middle.checkToken, v1handler.removeUser);
+route.delete('/users/:id', middle.checkToken, v1handler.removeUser);
 route.post('/login', middle.checkIfEmpty, v1handler.login);
 route.post('/users/register', middle.checkIfEmpty, middle.checkToken, v1handler.register);
 route.post('/logout', middle.checkIfEmpty, middle.checkToken, v1handler.logout);
